9db58bc16b3c26dacebee0083711b927301df6a0
Antonio D'Angelo <reapercsp@gmail.com>
2023-10-06 16:47:10 +0200

    mailer logic

---
+++ mailer.php
@@ -0,0 +1,61 @@
+<?php
+
+include 'db_connection.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; 
+
+$mail = new PHPMailer(true);
+
+if (isset($_POST["email"])) {
+    $email = $_POST["email"];
+
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER; Per fare vedere l'output dell'invio 
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'testmailer492@gmail.com';
+        $mail->Password   = 'hxzt paax xasn etqr';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        $mail->setFrom('testmailer492@gmail.com', 'Test Mailer');
+        $mail->addAddress($email);
+        $mail->addReplyTo('testmailer492@gmail.com', 'TESTMAILER');
+
+        $mail->isHTML(true);                                  
+        $mail->Subject = 'Reset Password';
+        $mail->Body    = 'Click <a href="http://localhost/task_for_edusogno/reset_pass.php">here</a> to reset to password.';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo "Il messaggio è stato invitato";
+        header("refresh:2;url=index.php");
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+} ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php if (!isset($_POST["email"])) { ?>
+        <form action="" method="post">
+            <label for="email">Invia un'email per cambiare la password</label>
+            <input name="email" placeholder="Inserisci l'indirizzo email">
+            <button type="submit">INVIA</button>
+        </form>
+    <?php } ?>
+</body>
---
+++ personal_homepage.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+include "db_connection.php";
+include "header.php";
+
+
+if (isset($_SESSION['id']) && isset($_SESSION['nome']) && isset($_SESSION['cognome']) && isset($_SESSION['email'])) {
+
+    $userEmail = $_SESSION['email'];
+    $sqlAdminCheck = "SELECT admin FROM utenti WHERE email = '$userEmail'";
+    $resultAdminCheck = mysqli_query($connection, $sqlAdminCheck);
+
+    if ($resultAdminCheck) {
+        $rowAdminCheck = mysqli_fetch_assoc($resultAdminCheck);
+        $isAdmin = $rowAdminCheck['admin'];
+    } else {
+        echo "Errore nella query di verifica admin: " . mysqli_error($conn);
+        exit();
+    }
+
+    if ($isAdmin) {
+        $sqlEvents = "SELECT * FROM eventi";
+    } else {
+        $sqlEvents = "SELECT * FROM eventi WHERE attendees LIKE '%$userEmail%'";
+    }
+
+    $resultEvents = mysqli_query($connection, $sqlEvents);
+
+    if ($resultEvents) {
+    } else {
+        echo "Errore nella query degli eventi: " . mysqli_error($conn);
+        exit();
+    }
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/styles/homeStyle.css">
+        <title>Home</title>
+    </head>
+
+    <body>
+        <h1>Ciao, <?php echo $_SESSION['nome']; ?> <?php echo $_SESSION['cognome']; ?> ecco i tuoi eventi</h1>
+        <div class="buttons">
+            <button><a href="logout.php">Logout</a></button>
+
+            <?php
+            if ($isAdmin) {
+            ?>
+                <button><a href="dash_admin.php">Pannello di Amministrazione</a></button>
+            <?php
+            } else {
+            ?>
+                <button><a href="javascript:void(0)" onclick="alert('Non sei un amministratore')">Pannello di Amministrazione</a></button>
+            <?php
+            }
+            ?>
+        </div>
+
+        <?php
+        if (mysqli_num_rows($resultEvents) > 0) {
+
+        ?> <div class="cards"> <?php
+                            while ($row = mysqli_fetch_assoc($resultEvents)) {
+                            ?>
+
+                    <div class="card-body">
+                        <h2 class="card-title"><?php echo $row['nome_evento']; ?></h2>
+                        <p class="card-text">Data: <?php echo $row['data_evento']; ?></p>
+                        <button>JOIN</button>
+                    </div>
+
+                <?php
+                            } ?>
+            </div> <?php
+                    mysqli_free_result($resultEvents);
+                } else {
+                    echo "Nessun evento";
+                }
+                    ?>
+
+    </body>
+
+    </html>
+<?php
+} else {
+}
+?>
