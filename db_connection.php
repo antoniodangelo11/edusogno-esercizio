f56fa18c91c1eaa1aad9fed5dcd780844f6d6cdf
Antonio D'Angelo <reapercsp@gmail.com>
2023-10-06 16:46:13 +0200

    add controller dashboard and db connection

---
+++ db_connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$server_name = "localhost";
+$user_name = "root";
+$password = "root";
+
+$db_name = "db_edu";
+
+$connection = mysqli_connect($server_name, $user_name, $password, $db_name);
+
+if (!$connection) {
+    echo "Connection failed!";
+}
